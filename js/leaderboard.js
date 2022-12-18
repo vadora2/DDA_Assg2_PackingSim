715c69dd832c2b26e24f1a4d0c05249705ac23ce
Melvyn <92712460+MelvynHoo@users.noreply.github.com>
2022-12-19 01:08:53 +0800

    unsuccessful leaderboard

--- js/leaderboard.js
+++ js/leaderboard.js
@@ -1,6 +1,6 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-app.js";
 import { getAuth, onAuthStateChanged} from "https://www.gstatic.com/firebasejs/9.14.0/firebase-auth.js";
-import { getDatabase, ref, get, child, set, onValue, orderByChild } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-database.js";
+import { getDatabase, ref, get, query, child, set, onValue, orderByChild, limitToLast} from "https://www.gstatic.com/firebasejs/9.14.0/firebase-database.js";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -20,7 +20,8 @@ const db = getDatabase();
 //Working with Auth
 const auth = getAuth();
 const user = auth.CurrentUser;
-const leaderboard = ref(db, "leaderboards");
+//const leaderboard = ref(db, "leaderboards");
+
 
 //Retrieve from login
 var myData = sessionStorage.getItem('UUID');
@@ -30,19 +31,32 @@ var limit = 10;
 
   getLB(limit);
   function getLB(limit = 10){
-    //q = get(lb).then(orderByChild("noOfMoneyEarned").LimitToLast(limit));
-    //lbList=[];
+     //q = get(leaderboard).then(orderByChild("noOfMoneyEarned"));
+    const que = query(ref(db,"leaderboards"),orderByChild("noOfMoneyEarned"),limitToLast(limit))
+    
   
-    get(leaderboard).then((snapshot) => { //retrieve a snapshot of the data using a callback
+    get(que).then((snapshot) => { //retrieve a snapshot of the data using a callback
       if (snapshot.exists()) {
         //if the data exist
+        /*
+        var lbList= [];
+
+        
+        snapshot.forEach((childSnapshot) => {
+           lbList.push(childSnapshot.val());                   
+        });
+        lbList.reverse()
+        console.log(lbList);
+        //AddAllItemsToTable(lbList);
+        */
         
         try {
           //let's do something about it
           var overLeaderBoard = document.getElementById("leaderBoardData");
           var content = "";
           snapshot.forEach((childSnapshot) => {
-            //console.log(childSnapshot);
+            
+            
             //looping through each snapshot
             console.log(`username of players found: ${childSnapshot.child("userName").val()}`);
             content += `<tr>
@@ -66,7 +80,21 @@ var limit = 10;
       }
     });
   }
-
+/*
+  function AddAllItemsToTable(leaderboard)
+  {
+    leaderboard.reverse();
+    console.log(leaderboard);
+    var overLeaderBoard = document.getElementById("leaderBoardData");
+    var content, index1, index2, index3, index4 = "";
+    for (let i = 0; i++; index1 in leaderboard.i) {
+      
+      content += "<td>"+ leaderboard.i[index1].noOfMoneyEarned + "</td>";
+    }
+    console.log(content);
+    overLeaderBoard.innerHTML = content;
+  }
   function GetCurrentUser(){
     return user;
   }
+  */
